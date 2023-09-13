63ef8e91bd4755d4b33df011bb86408f2a35b0b0
송 유진 <tlahsh12@naver.com>
2023-09-13 23:54:01 +0900

    지연 로딩 추가

--- src/main/java/entity/Member.java
+++ src/main/java/entity/Member.java
@@ -22,8 +22,20 @@ public class Member {
     @Id
     private String id;
 
+    // 즉시 로딩
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "TEAM_ID")
+    // 조인 컬럼이 NULLABLE 이라면 자체적으로 외부 조인을 하게 됨.
+    // 외부 조인보다 내부 조인이 성능과 최적화에 유리하므로, 원하는 경우 외래 키에 NOT NULL 제약 조건을 설정하여, 내부 조인을 유도한다.
+    // @JoinColumn(name = "TEAM_ID", nullable = false)
+    // 또는 @JoinColumn(name = "TEAM_ID", optional = false)
     private Team team;
+
+    // 지연 로딩
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "TEAM_ID")
+    // em.find(Member.class, "member1")를 호출하면 회원만 조회하고, 팀은 조회하지 않는다.
+    // 조회한 회원의 team 멤버변수에 프록시 객체를 넣어 둔다.
+    private Team team2;
 
 }
